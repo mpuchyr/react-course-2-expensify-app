a00cb4a83a73d1146e87c15867a6f9e5be777107
mpuchyr <matt.puchyr@gmail.com>
2020-12-11 16:16:15 -0600

    Setup ExpensesSummary component

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 export const ExpenseDashboardPage = () => (
     <div>
         <ExpenseListFilters />
+        <ExpensesSummary />
         <ExpenseList />
+        
     </div>
 );
 
